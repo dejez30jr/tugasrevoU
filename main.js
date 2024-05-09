6bf7045faee76a8ff035ad38a7d91658cca6011f
dejez30jr <165728707+dejez30jr@users.noreply.github.com>
2024-05-09 15:28:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ const menulist = document.getElementById("menu-list")
 menuIcon.addEventListener("click", ()=>{
     menulist.classList.toggle("hidden")
 })
+
+let nama = prompt('siapa namamu?')
+alert('oh nama kamu ' + nama)
+alert('welcome to website saya')
